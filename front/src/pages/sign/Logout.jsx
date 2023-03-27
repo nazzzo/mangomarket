4b9de6d4cebddad41508adf09c70168d2249c70d
kimjh <hecticika@gmail.com>
2023-03-27 16:23:15 +0900

    fix: github action

--- front/src/pages/sign/Logout.jsx
+++ front/src/pages/sign/Logout.jsx
@@ -11,5 +11,5 @@ export const Logout = () => {
     dispatch(userLogout());
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
     navigate("/");
-  }, []);
+  }, [dispatch, navigate]);
 };
