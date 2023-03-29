f192db8e0b29af9a50bb9161d88b2879efb4d139
char1ey <ckstn410@gmail.com>
2023-03-29 10:43:46 +0900

    feat Header.SearchPopUp

--- front/src/common/header/Header.jsx
+++ front/src/common/header/Header.jsx
@@ -1,33 +1,12 @@
+import { useState } from "react"
 import { Navigate, NavLink, useNavigate } from "react-router-dom";
-import { HeaderWrapper, HeaderWrap, HeaderLogoWrap, HeaderLogoImgWrap, HeaderLogoImg, HeaderMenuWrap, HeaderMenuul, HeaderMenuli, HeaderFunctionWrap, HeaderSearchWrap, HeaderSearchBox, HeaderSearchInput, HeaderSearchIcon, HeaderAlarmWrap, HeaderAlarm, HeaderUserWrap, HeaderHamburger, HeaderUser } from "./styled"
-import { Hamburger } from '../hamburger';
-
-import styled from 'styled-components';
-
-const SearchPopUp = styled.div`
-    display: none;
-
-    @media screen and (max-width: 480px){
-        display: block;
-        width: 30rem;
-        height: 10rem;
-        background-color: #cecece;
-    }
-`
-
-const SearchPopInput = styled.input`
-    @media screen and (max-width: 480px){
-        width: 100%;
-        height: inherit;
-        border: none;
-        border-radius: 4px;
-        background-color: #efefef;
-        box-sizing: border-box;
-        padding: 0 0.5rem;
-    }
-`
+import { HeaderWrapper, HeaderWrap, HeaderLogoWrap, HeaderLogoImgWrap, HeaderLogoImg, HeaderMenuWrap, HeaderMenuul, HeaderMenuli, HeaderFunctionWrap, HeaderSearchWrap, HeaderSearchBox, HeaderSearchInput, HeaderSearchIcon, HeaderAlarmWrap, HeaderAlarm, HeaderUserWrap, HeaderUser } from "./styled"
+import { Hamburger, SearchPopUp } from '../index';
 
 export const Header = (({ categories, isLogin, user }) => {
+    const [ searchBox, setSearchBox ] = useState(false)
+    const [ menuBox, setMenuBox ] = useState(false)
+
     const navigate = useNavigate()
 
     const loginFilter = categories.filter( v => v.isLogin === null || v.isLogin === isLogin )
@@ -40,25 +19,46 @@ export const Header = (({ categories, isLogin, user }) => {
                 </HeaderMenuli>
             )})
 
+    const searchClickHandler = () => {
+        if(menuBox) {
+            setMenuBox(!menuBox)
+            setSearchBox(!searchBox)}
+        else {
+            setSearchBox(!searchBox) 
+        }
+    }
+
+    const menuClickHandler = () => {
+        if(searchBox) {
+            setSearchBox(!searchBox)
+            setMenuBox(!menuBox)}
+        else {
+            setMenuBox(!menuBox) 
+        }
+    }
+
     return(
             <>
                 <HeaderWrapper>
                     <HeaderWrap>
+                        {/*  */}
                         <HeaderLogoWrap>
                             <HeaderLogoImgWrap onClick={() => {navigate("/")}}>
                                 <HeaderLogoImg src='./mango.png'/>
                             </HeaderLogoImgWrap>
                         </HeaderLogoWrap>
+                        {/*  */}
                         <HeaderMenuWrap>
                             <HeaderMenuul>
                                 {navigation}
                             </HeaderMenuul>
                         </HeaderMenuWrap>
+                        {/*  */}
                         <HeaderFunctionWrap>
                             <HeaderSearchWrap>
                                 <HeaderSearchBox>
                                     <HeaderSearchInput placeholder='검색어를 입력해주세요' type={'search'}/>
-                                    <HeaderSearchIcon src='./search.png'/>
+                                    <HeaderSearchIcon src='./search.png' onClick={searchClickHandler}/>
                                 </HeaderSearchBox>
                             </HeaderSearchWrap>
                             <HeaderAlarmWrap>
@@ -69,11 +69,10 @@ export const Header = (({ categories, isLogin, user }) => {
                                 <Hamburger />
                             </HeaderUserWrap>
                         </HeaderFunctionWrap>
+                        {/*  */}
                     </HeaderWrap>
                 </HeaderWrapper>
-                <SearchPopUp>
-                    <SearchPopInput />
-                </SearchPopUp>
+                <SearchPopUp visible={searchBox}/>
             </>
         )
     }
