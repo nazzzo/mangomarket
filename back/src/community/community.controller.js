98ed73214fd46567b4aabeb878d3c7276c44932f
hongtae <ambious12@gmail.com>
2023-03-31 16:42:53 +0900

    feat: test

--- back/src/community/community.controller.js
+++ back/src/community/community.controller.js
@@ -2,6 +2,38 @@ class CommunityController {
     constructor({ communityService }) {
         this.communityService = communityService
     }
+
+    // async putComment(req, res, next){
+    //     try{
+    //         console.log(req.params)
+    //         console.log(req.body)
+    //     } catch(e){
+    //         next(e)
+    //     }
+    // }
+
+    async getComment(req, res, next) {
+        try {
+            const { id, idx } = req.params
+            const response = await this.communityService.getComment({ id, idx })
+            res.json(response)
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    async postComment(req, res, next) {
+        try {
+            const { id } = req.params
+            const { content } = req.body
+            if (!content) throw new Error('내용을 입력해주세요')
+            const response = await this.communityService.postComment({ id, content })
+            res.json(response)
+        } catch (e) {
+            next(e)
+        }
+    }
+
     async getWriting(req, res, next) {
         try {
             const { id } = req.params
@@ -38,7 +70,6 @@ class CommunityController {
         try {
             if (!req.body.content) throw new Error('내용이 없습니다')
             const { email, content, subject } = req.body
-            console.log(`req.body :`, { email, content, subject })
             const response = await this.communityService.postCommunity({
                 email,
                 content,
@@ -49,13 +80,12 @@ class CommunityController {
             next(e)
         }
     }
+
     async putCommunity(req, res, next) {
         console.log(`req.body :`, req.body)
         try {
             if (!req.body.subject || !req.body.content)
                 throw new Error('수정할 내용을 입력해주세요')
-
-            // console.log(`putController:`, req.params.id, req.body.content);
             const response = await this.communityService.putCommunity(
                 req.params.id,
                 req.body.content,
@@ -66,11 +96,44 @@ class CommunityController {
             next(e)
         }
     }
+
+    async putComment(req, res, next) {
+        console.log(`req.body :`, req.params, req.body)
+
+        try {
+            if (!req.body.subject || !req.body.content)
+                throw new Error('수정할 내용을 입력해주세요')
+
+            const response = await this.communityService.putComment(
+                req.params.id,
+                req.params.idx,
+                req.body.content
+            )
+            res.status(201).json(response)
+        } catch (e) {
+            next(e)
+        }
+    }
+
     async deleteCommunity(req, res, next) {
         try {
             // if (!req.params.id) throw new Error("삭제할 댓글이 없습니다");
             console.log('req.params.id', req.params.id)
             const response = await this.communityService.deleteCommunity(req.params.id)
+            res.status(201).json(response)
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    async deleteComment(req, res, next) {
+        try {
+            // if (!req.params.id) throw new Error("삭제할 댓글이 없습니다");
+            console.log(req.params.id, req.params.idx)
+            const response = await this.communityService.deleteComment(
+                req.params.id,
+                req.params.idx
+            )
             res.status(201).json(response)
         } catch (e) {
             next(e)
