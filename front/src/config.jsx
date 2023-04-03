01ff424f2a0ed768f2c2b2f1ec1ab50e0dac9b7a
kimjh <hecticika@gmail.com>
2023-04-03 12:47:08 +0900

    fix kakao login

--- front/src/config.jsx
+++ front/src/config.jsx
@@ -1,20 +1,20 @@
-// const config = {
-//     PT: "https",
-//     HOST: 'www.mgmarket.store',
-//     PORT: 3000,
-//     BACKEND_PORT: 443,
-//     IMG_PORT: 3005,
-// }
-// export default config
-
 const config = {
-    PT: "http",
-    HOST: "localhost",
+    PT: "https",
+    HOST: 'mgmarket.store',
     PORT: 3000,
-    BACKEND_PORT: 3005,
+    BACKEND_PORT: 443,
     IMG_PORT: 3005,
 }
 export default config
+
+// const config = {
+//     PT: "http",
+//     HOST: "localhost",
+//     PORT: 3000,
+//     BACKEND_PORT: 3005,
+//     IMG_PORT: 3005,
+// }
+// export default config
 
 
 // PORT=3000
