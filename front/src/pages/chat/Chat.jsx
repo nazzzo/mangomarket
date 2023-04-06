6013931014e4183c77887f7438aa875a717130ed
char1ey <ckstn410@gmail.com>
2023-04-06 17:04:48 +0900

    feat: useChat 파일생성

--- front/src/pages/chat/Chat.jsx
+++ front/src/pages/chat/Chat.jsx
@@ -1,65 +1,34 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from "react-redux";
-import { useInput } from "../../hooks"
-import io from "socket.io-client"
-import config from "../../config"
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useChat, useInput } from "../../hooks"
 
+export const Chat = ({ seller, boardId }) => {
+    const content = useInput()
+    const [ messages, setMessages ] = useState([])
+    const { user } = useSelector((state) => state.user)
 
-export const Chat = ({ receiver, boardId }) => {
-    const content = useInput("")
-    const { user } = useSelector((state) => state.user);
-    const [ socketOn, setSocketOn ] = useState()
-    const [ text, setText ] = useState("")
-    // console.log(`sender:::`, user) // 구매자
-    // console.log(`writer(receiver):::`, receiver) // 판매자
-    // console.log(boardId) // 판매글번호
+    const { sendMessage } = useChat({ seller, user, boardId, setMessages })
 
-    const socket = io(`${config.PT}://${config.HOST}:${config.BACKEND_PORT}/`)
-    socket.emit("init", { namespace: boardId, room: user.email, username: user.username })
-    
-    useEffect(() => {
-        // back-server 주소
-        const namespace = io(`${config.PT}://${config.HOST}:${config.BACKEND_PORT}/${boardId}`)
-        // setSocketOn(socket)
-        // setSocketOn(namespace)
-        // namespace.emit('join', { data: `${user.email}`})
-        
-        return () => {
-            socket.disconnect();
-        }
-    },[])
-    
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(socketOn)
-        socketOn.emit(`${user.email}`, { message: content.value} )
-        socketOn.on("message", (data) => {
-            console.log(data)
-            setText(data.message)
-        })
-
-
-        // namespace.on(`${user.email}`, (data) => {
-        //     console.log(data)
-        // })
-
-        // socket.emit(`${user.email}`, {
-        //     content: content.value,
-        //     seller: `${user.email}`,
-        //     customer: `${receiver.email}`,
-        //     type: `sender`,
-        //     boardid: boardId,
-        // })
-        
+        sendMessage(content.value)
     }
 
     return (
-        <>
+        <div>
+            {/* Render chat messages */}
+            {messages && messages.map((message, index) => (
+                <div key={index}>
+                    <strong>{message.sender}: </strong>
+                    {message.content}
+                </div>
+            ))}
+
+            {/* Render chat input form */}
             <form onSubmit={handleSubmit}>
-                <input onChange={content.onChange} value={content.value} type="text" name="content" id="content"/>
-                <button type='submit'>채팅</button>
+                <input type="text" {...content} />
+                <button type="submit">Send</button>
             </form>
-            <div>{text}</div>
-        </>
+        </div>
     )
 }
