a45c2b5fa5e14f97f41c74fcf5f2db60faa731c6
kimjh <hecticika@gmail.com>
2023-04-06 09:48:48 +0900

    fix

--- back/server.js
+++ back/server.js
@@ -1,9 +1,10 @@
 const app = require('./app.js')
+// const socketIO = require("./socket.js")
 const { sequelize } = require('./models')
 const config = require('./config')
 const PORT = config.port
 
-app.listen(PORT, async () => {
+const http = app.listen(PORT, async () => {
     await sequelize.sync({ force: false })
 
 
@@ -30,3 +31,5 @@ app.listen(PORT, async () => {
 
     console.log(`backend server listening on port ${PORT}`)
 })
+
+// socketIO(http, app)
