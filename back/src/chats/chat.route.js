11dac757a99e2ecb467572bc6ea09929ad701089
char1ey <ckstn410@gmail.com>
2023-04-07 15:35:15 +0900

    feat: postChat 라우터 생성

--- back/src/chats/chat.route.js
+++ back/src/chats/chat.route.js
@@ -5,5 +5,5 @@ const { controller } = require("./chat.module")
 
 router.get("/", (req, res, next) => controller.getList(req, res, next));
 // router.post("/:id/:idx", (req, res, next) => controller.postChat(req, res, next));
-
+router.post("/", (req, res, next) => controller.postChat(req, res, next))
 module.exports = router
