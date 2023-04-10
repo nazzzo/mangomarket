de26fd5d7de534da4f9e3d7c9dd27b039d212652
kimjh <hecticika@gmail.com>
2023-04-10 17:08:07 +0900

    fix chatter list

--- back/src/chats/chat.repository.js
+++ back/src/chats/chat.repository.js
@@ -7,16 +7,30 @@ class ChatRepository {
   }
   async findAll(type) {
     try {
-      console.log(type)
+      console.log(type);
       if (type.customer) {
-        const findAll = await this.Chat.findAll({ where:{ customer:`${type.customer}`,seller: `${type.opponent}`, boardid: `${type.boardid}`}, raw: true, nest: true});
+        const findAll = await this.Chat.findAll({
+          where: {
+            customer: `${type.customer}`,
+            seller: `${type.opponent}`,
+            boardid: `${type.boardid}`,
+          },
+          raw: true,
+          nest: true,
+        });
+        return findAll;
+      } else if (type.seller) {
+        const findAll = await this.Chat.findAll({
+          where: {
+            seller: `${type.seller}`,
+            customer: `${type.opponent}`,
+            boardid: `${type.boardid}`,
+          },
+          raw: true,
+          nest: true,
+        });
         return findAll;
       }
-      else if (type.seller) {
-        const findAll = await this.Chat.findAll({ where:{ seller:`${type.seller}`,customer: `${type.opponent}`, boardid: `${type.boardid}`}, raw: true, nest: true});
-        return findAll;
-      }
-
     } catch (e) {
       throw new Error(e);
     }
@@ -25,18 +39,18 @@ class ChatRepository {
   async postChat(data) {
     try {
       const result = await this.Chat.create(data);
-      return result
+      return result;
     } catch (e) {
-      throw new Error(e)
+      throw new Error(e);
     }
   }
 
   async getUsers({ type, useremail }) {
     try {
-      let column
-      console.log(type) // seller ? customer
-      console.log(useremail)
-      type === "seller" ? column = `customer` : column = `seller`
+      let column;
+      console.log(type); // seller ? customer
+      console.log(useremail);
+      type === "seller" ? (column = `customer`) : (column = `seller`);
 
       const sql = `SELECT 
       A.${column}, 
@@ -45,30 +59,29 @@ class ChatRepository {
       B.userImg,
       B.address,
       C.image,
-      A.content,
-      A.createdAt
-      FROM (
-        SELECT 
-            ${column}, 
-            boardid, 
-            MAX(content) as content,
-            MAX(createdAt) as createdAt
-        FROM Chat 
-        WHERE ${type} = "${useremail}"
-        GROUP BY ${column}, boardid
-      ) AS A 
-      JOIN User as B 
-          ON A.${column} = B.email
-      JOIN BoardImage as C
-          ON A.boardid = C.boardid
-      WHERE
-          C.thumbnail = 1     
+      A.content
+      FROM Chat AS A
+      INNER JOIN (
+      SELECT 
+          ${column}, 
+          boardid, 
+          MAX(createdAt) AS max_createdAt
+      FROM Chat
+      WHERE ${type} = "${useremail}"
+      GROUP BY ${column}, boardid
+      ) AS D ON A.${column} = D.${column} AND A.boardid = D.boardid AND A.createdAt = D.max_createdAt
+      JOIN User AS B 
+      ON A.${column} = B.email
+      JOIN BoardImage AS C
+      ON A.boardid = C.boardid     
+      WHERE 
+      C.thumbnail = 1
       ORDER BY 
-          A.createdAt DESC;`
-      const result = await this.sequelize.query(sql, { raw: true, nest: true })
-      return result
+      A.createdAt DESC;`;
+      const result = await this.sequelize.query(sql, { raw: true, nest: true });
+      return result;
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
 }
