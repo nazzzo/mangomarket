a8299d927cb06e696618a9d544163b2403049038
char1ey <ckstn410@gmail.com>
2023-04-11 20:47:13 +0900

    feat: socket.js reservation 이벤트 추가

--- back/socket.js
+++ back/socket.js
@@ -2,37 +2,52 @@ const SocketIO = require("socket.io")
 const { repository } = require("./src/chats/chat.module")
 
 module.exports = (server, app) => {
-    try {
-        const io = SocketIO(server, { cors: { origin: '*' } })
-        io.on("connection", (socket) => {
-            console.log("user connected");
+  try {
+    const io = SocketIO(server, { cors: { origin: '*' } })
+    io.on("connection", (socket) => {
+      console.log("user connected");
 
-            let roomname
-            socket.on("joinRoom", ({ room }) => {
-                socket.join(room);
-                roomname = room
-                console.log(`room:::`, room);
-              });
-              socket.on("sendMessage", ({ data }) => {
-                const date = new Date()
-                io.to(roomname).emit("receiveMessage", {
-                  boardid: data.boardid,
-                  seller: data.seller,
-                  customer: data.customer,
-                  content: data.content,
-                  email: data.email,
-                  username: data.username,
-                  userImg: data.userImg,
-                  address: data.address,
-                  createdAt: date.toISOString(),
-                })
-            })
+      let roomname
+      socket.on("joinRoom", ({ room }) => {
+        socket.join(room);
+        roomname = room
+        console.log(`room:::`, room);
+      });
 
-            socket.on("disconnect", () => {
-                console.log(`disconnected`)
-              })
+      socket.on("sendMessage", ({ data }) => {
+        const date = new Date()
+        io.to(roomname).emit("receiveMessage", {
+          boardid: data.boardid,
+          seller: data.seller,
+          customer: data.customer,
+          content: data.content,
+          email: data.email,
+          username: data.username,
+          userImg: data.userImg,
+          address: data.address,
+          createdAt: date.toISOString(),
         })
-    } catch (e) {
-            console.log(e)
-    }
+      })
+
+      socket.on("reservation", ({ data }) => {
+        io.to(roomname).emit("reserve", {
+          // boardid: data.boardid,
+          // seller: data.seller,
+          // customer: data.customer,
+          // content: data.content,
+          // email: data.email,
+          // username: data.username,
+          // userImg: data.userImg,
+          // address: data.address,
+          // createdAt: date.toISOString(),
+        })
+      })
+
+      socket.on("disconnect", () => {
+        console.log(`disconnected`)
+      })
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }
--- front/src/config.jsx
+++ front/src/config.jsx
@@ -1,14 +1,3 @@
-const config = {
-    PT: "https",
-    HOST: 'www.mgmarket.store',
-    REDIRECT_HOST: 'mgmarket.store',
-    PORT: 3000,
-    BACKEND_PORT: 443,
-    IMG_PORT: 443,
-}
-
-export default config
-
 // const config = {
 //     PT: "https",
 //     HOST: 'www.mgmarket.store',
