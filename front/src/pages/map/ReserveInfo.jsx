93155e9fc86b2623450aaace9419a48c07c15826
kimjh <hecticika@gmail.com>
2023-04-12 14:01:37 +0900

    fix chat reservation

--- front/src/pages/map/ReserveInfo.jsx
+++ front/src/pages/map/ReserveInfo.jsx
@@ -1,12 +1,16 @@
 import { useState,useEffect } from "react";
+import { useSelector } from "react-redux"
 import { ReserveInfoForm, AddressInfo, TimeInfo } from "./styled"
 import { Button } from "../../common/button"
 import { Alert } from "../../common/alert"
 import request from "../../utils/request"
 
 
-export const ReserveInfo = ({ address, time }) => {
+export const ReserveInfo = ({ address, time, boardid, customer, seller }) => {
   const [isOpenAlert, setIsOpenAlert] = useState(false);
+  const { user } = useSelector((state) => state.user)
+
+  console.log(`user-customer-seller-boardid:::`, user.email, customer, seller, boardid )
 
   const date = new Date(time);
   const formattedTime = date.toLocaleDateString('en-US', {hour: 'numeric', minute: 'numeric'});
@@ -14,8 +18,10 @@ export const ReserveInfo = ({ address, time }) => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-        const response = await request.put("/board/${id}", {
+        const response = await request.put(`/board/${boardid}/state`, {
+            state: "reserved"
           });
+          console.log(`response.data:::`, response.data)
         if (response.data === 1) setIsOpenAlert(true)
     } catch (e) { console.error(e)}
   }
